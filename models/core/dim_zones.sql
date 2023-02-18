ccd276e427020dd597fcba37e6bea55f685ec2d8
sanyashireen@gmail.com <sanyashireen@gmail.com>
2023-02-18 06:55:11 +0000

    CICI: Fixed clumn names in dim_zones.sql

--- models/core/dim_zones.sql
+++ models/core/dim_zones.sql
@@ -1,7 +1,7 @@
 {{config(materialized='table')}}
 
 select 
-    LocationId as locationid,
+    LocationID as locationid,
     Borough as borough,
     Zone as zone,
     replace(service_zone, 'Boro', 'Green') as service_zone
